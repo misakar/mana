fd704b5160ab95b7d3803d6618e010b64d6c82c9
neo1218 <neo1218@yeah.net>
2016-01-04 22:29:58 +0800

    clean

--- tests/test2/app/hello/views.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from . import hello
-from flask import render_template
-
-
-# test views
-@hello.route('/test/')
-def test():
-    return "<h1>just tell you everything is ok!</h1>"
--- tests/test2/app/models.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-from . import db
-
-# write your database model here
-
